691c7f04c91e1385e67d833fee99121992faafea
alicinia <alicinia97@gmail.com>
2021-10-22 21:39:30 +0700

    JS fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 function collapseText(btn) {
-    let target = document.querySelector(btn.getAttribute('data-target'));
-    let showIndex = target.className.indexOf('show');
-    let className = target.className
-    if (showIndex > -1) {
-        target.className = className.substr(0, showIndex) + 'collapse';
-    } else {
-        target.className = className.substr(0, className.indexOf('collapse')) + 'show';
+    let target = document.querySelector(btn.dataset.target);
+
+    if (target) {
+        const isShown = target.classList.contains('show');
+        target.classList.toggle('show', !isShown);
+        target.classList.toggle('collapse', isShown);
     }
 }
